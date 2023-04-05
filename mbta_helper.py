8883a0b68ac5dc2cc6c1f340d932a81d91d98844
pengrulin <plin3@babson.edu>
2023-04-04 21:36:05 -0400

    1

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,10 +1,13 @@
 # Your API KEYS (you need to use your own keys - very long random characters)
-from config import MAPBOX_TOKEN, MBTA_API_KEY
+
+
+
 
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
+
 
 
 # A little bit of scaffolding if you want to use it
